647aec659582c15638fd9c913713bb0b2ab8e662
Nurzhan Bizhan <bizhannk@gmail.com>
2023-05-12 15:48:59 +0600

    add styles and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import './App.css';
+import GlobalStyles from './components/styles/Global';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import InnerPage from './pages/InnerPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/inner" element={<InnerPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
