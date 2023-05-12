598f70b6c9e8182abbceb56feefa2582ecfcbeb5
Nurzhan Bizhan <bizhannk@gmail.com>
2023-05-12 18:54:27 +0600

    create navlink constgants

---
+++ src/utils/navlinks_constants.js.js
@@ -0,0 +1,23 @@
+import {
+  faUser,
+  faGraduationCap,
+  faPencil,
+  faGem,
+  faSuitcase,
+  faLocationArrow,
+  faComment,
+} from '@fortawesome/free-solid-svg-icons';
+
+export const navlinks = [
+  { icon: faUser, title: 'about me', id: '#about-me' },
+  { icon: faGraduationCap, title: 'education', id: '#education' },
+  {
+    icon: faPencil,
+    title: 'experience',
+    id: '#experience',
+  },
+  { icon: faGem, title: 'skills', id: '#skills' },
+  { icon: faSuitcase, title: 'portfolio', id: '#portfolio' },
+  { icon: faLocationArrow, title: 'contacts', id: '#contacts' },
+  { icon: faComment, title: 'feedbacks', id: '#feedbacks' },
+];
