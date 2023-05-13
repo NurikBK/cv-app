9c5ba75f2490b92689c4c9ed188e41688b3021c5
Nurzhan Bizhan <bizhannk@gmail.com>
2023-05-13 12:59:21 +0600

    add constants

---
+++ src/constants/contactsArr.js
@@ -0,0 +1,39 @@
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+  faTwitter,
+  faFacebookF,
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons';
+
+export const contacts = [
+  {
+    icon: faPhone,
+    title: '500 342 242',
+    info: '',
+    link: 'tel:+500 342 242',
+  },
+  {
+    icon: faEnvelope,
+    title: 'office@kamsolutions.pl',
+    info: '',
+    link: 'mailto:office@kamsolutions.pl',
+  },
+  {
+    icon: faTwitter,
+    title: 'Twitter',
+    info: 'www.twitter.com/wordpress',
+    link: 'https://twitter.com/wordpress',
+  },
+  {
+    icon: faFacebookF,
+    title: 'Facebook',
+    info: 'www.facebook.com/facebook',
+    link: 'https://www.facebook.com/facebook',
+  },
+  {
+    icon: faSkype,
+    title: 'Skype',
+    info: 'kamsolutions.pl',
+    link: 'skype:kamsolutions.pl',
+  },
+];
---
+++ src/constants/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+  colors: {
+    accent: '#26c17e',
+    secondary: '#fff',
+    dark: '#667081',
+    darkBg: '#222935',
+  },
+};
