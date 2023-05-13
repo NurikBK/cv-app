53cd4790f00ed87b1dbcc328ce97c3c631e5fa7d
Nurzhan Bizhan <bizhannk@gmail.com>
2023-05-13 09:48:10 +0600

    add theme

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import GlobalStyles from './components/styles/Global';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import InnerPage from './pages/InnerPage';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './utils/theme';
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/inner" element={<InnerPage />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/inner" element={<InnerPage />} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
