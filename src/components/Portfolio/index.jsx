9091d43eaaa9a316c2b09ffcd6d842afa93a3612
Nurzhan Bizhan <bizhannk@gmail.com>
2023-05-13 17:31:52 +0600

    fix layout with isotope

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,47 +1,48 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { StyledPortfolio } from '../styles/Portfolio.styled';
+import Isotope from 'isotope-layout';
 import project1Img from '../../assets/project1.png';
 
 const Portfolio = () => {
+  // init one ref to store the future isotope object
+  const isotope = useRef();
+  // store the filter keyword in a state
+  const [filterKey, setFilterKey] = useState('*');
+
+  // initialize an Isotope object with configs
+  useEffect(() => {
+    isotope.current = new Isotope('.filter-container', {
+      itemSelector: '.filter-item',
+      layoutMode: 'fitRows',
+    });
+    // cleanup
+    return () => isotope.current.destroy();
+  }, []);
+
+  // handling filter key change
+  useEffect(() => {
+    filterKey === '*'
+      ? isotope.current.arrange({ filter: `*` })
+      : isotope.current.arrange({ filter: `.${filterKey}` });
+  }, [filterKey]);
+
+  const handleFilterKeyChange = (key) => () => setFilterKey(key);
   return (
     <StyledPortfolio>
-      <ul className="tabs">
-        <li className="active">
-          <span> all </span>
-        </li>
-        <li className="">
-          <span> ui </span>
-        </li>
-        <li className="">
-          <span> code </span>
-        </li>
-      </ul>
-      <ul
-        className="filter-container"
-        style={{ position: 'relative', height: '428px' }}
-      >
-        <li
-          className="filter-item ui"
-          style={{ position: 'absolute', left: '40px', top: '0' }}
-        >
-          <img src={project1Img} alt="Space travel site" />
-          <div className="portfolio-info">
-            <h2>Space Travel</h2>
-            <p>In this project I used HTML, CSS and JS</p>
-            <a
-              href="https://nurikbk.github.io/space-travel-site/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              View source
-            </a>
-          </div>
-        </li>
-        <li
-          className="filter-item code"
-          style={{ position: 'absolute', left: '368px', top: '0px' }}
-        >
-          <img src="static/media/card_3.608e2b77.png" alt="card" />
+      <div className="tabs">
+        <button className="active" onClick={handleFilterKeyChange('*')}>
+          All
+        </button>
+        <button className="" onClick={handleFilterKeyChange('ui')}>
+          UI
+        </button>
+        <button className="active" onClick={handleFilterKeyChange('code')}>
+          Code
+        </button>
+      </div>
+      <ul className="filter-container">
+        <li className="filter-item ui">
+          <img src={project1Img} alt="card" />
           <div className="portfolio-info">
             <h2>Some text</h2>
             <p>
@@ -54,11 +55,8 @@ const Portfolio = () => {
             </a>
           </div>
         </li>
-        <li
-          className="filter-item ui"
-          style={{ position: 'absolute', left: '40px', top: '214px' }}
-        >
-          <img src="static/media/card_1.b1844ee5.jpg" alt="card" />
+        <li className="filter-item code">
+          <img src={project1Img} alt="card" />
           <div className="portfolio-info">
             <h2>Some text</h2>
             <p>
@@ -71,11 +69,22 @@ const Portfolio = () => {
             </a>
           </div>
         </li>
-        <li
-          className="filter-item code"
-          style={{ position: 'absolute', left: '368px', top: '214px' }}
-        >
-          <img src="static/media/card_3.608e2b77.png" alt="card" />
+        <li className="filter-item ui">
+          <img src={project1Img} alt="card" />
+          <div className="portfolio-info">
+            <h2>Some text</h2>
+            <p>
+              Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+              arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae,
+              justo. Nullam dictum felis eu pede mollis{' '}
+            </p>
+            <a href="https://somesite.com" target="_blank" rel="noreferrer">
+              View source
+            </a>
+          </div>
+        </li>
+        <li className="filter-item code">
+          <img src={project1Img} alt="card" />
           <div className="portfolio-info">
             <h2>Some text</h2>
             <p>
