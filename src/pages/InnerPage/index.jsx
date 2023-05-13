2eb9df7a0f5cb6e1dedc3e683bfdf2a48cc18ef3
Nurzhan Bizhan <bizhannk@gmail.com>
2023-05-13 15:56:54 +0600

    add props

--- src/pages/InnerPage/index.jsx
+++ src/pages/InnerPage/index.jsx
@@ -11,6 +11,8 @@ import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import { HashLink } from 'react-router-hash-link';
 import { Container } from '../../components/styles/Container.styled';
 import { timelineData } from '../../constants/timelineData';
+import { expertiseData } from '../../constants/expertiseData';
+import { feedbackData } from '../../constants/feedbacksData';
 
 const InnerPage = () => {
   return (
@@ -19,29 +21,38 @@ const InnerPage = () => {
       <main>
         <Box title="About me" id="about-me">
           <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
-            aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
-            mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum
-            semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-            porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem
-            ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra
-            nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-            Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies
-            nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget
-            condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem
-            neque
+            Hi, my name is Nurzhan and I am a Junior Web Developer. I have
+            experience in HTML, CSS, SCSS, JavaScript, React, NextJS, and MUI.
+            I'm excited to be here and I would like to tell you a little bit
+            more about my skills and experience. I am proficient in HTML, and I
+            have experience in creating and formatting web pages using semantic
+            markup. I am also skilled in CSS and SCSS, and I can create
+            responsive layouts and custom styles that enhance the look and feel
+            of web pages. In addition, I have experience in working with CSS
+            frameworks such as Material UI to create modern and dynamic web
+            designs. My JavaScript skills are strong, and I am proficient in
+            using JavaScript to create dynamic user interfaces and web
+            applications. I have experience in creating interactive web
+            applications using React and NextJS, and I am familiar with React's
+            component-based architecture and the NextJS server-side rendering
+            framework. Furthermore, I am skilled in using Material UI to create
+            modern and responsive user interfaces. I have experience in creating
+            reusable React components and integrating them with Material UI to
+            create consistent and cohesive user interfaces. I am a quick learner
+            and enjoy staying up-to-date with the latest web development
+            technologies and frameworks. I am also passionate about
+            collaborating with other developers and designers to create
+            high-quality web projects that meet user needs. Thank you for
+            considering my skills and experience for your web development
+            projects. I am excited to work with you to create dynamic and
+            engaging web applications.
           </p>
         </Box>
         <Box title="Education" id="education">
           <Timeline data={timelineData} />
         </Box>
         <Box title="Experience" id="experience">
-          <Expertise />
+          <Expertise data={expertiseData} />
         </Box>
         <Box title="Skills" id="skills"></Box>
         <Box title="Portfolio" id="portfolio">
@@ -51,7 +62,7 @@ const InnerPage = () => {
           <Address />
         </Box>
         <Box title="Feedbacks" id="feedbacks">
-          <Feedback />
+          <Feedback data={feedbackData} />
         </Box>
       </main>
       <HashLink to={'#top'} className="btn-fixed">
