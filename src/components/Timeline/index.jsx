3b67b8ccdf966a36095501bc3c039518f21a92b3
Nurzhan Bizhan <bizhannk@gmail.com>
2023-05-13 13:00:37 +0600

    add props

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import { StyledTimeline } from '../styles/Timeline.styled';
 
-const Timeline = () => {
+const Timeline = ({ data }) => {
   return (
     <StyledTimeline>
       <ul className="timeline-list">
-        <li>
-          <div className="timeline-date">2001</div>
-          <div className="general-event timeline-event">
-            <div className="info">
-              <h3>Title 0</h3>
-              <p>
-                Elit voluptate ad nostrud laboris. Elit incididunt mollit enim
-                enim id id laboris dolore et et mollit. Mollit adipisicing
-                ullamco exercitation ullamco proident aute enim nisi. Dolore eu
-                fugiat consectetur nulla sunt Lorem ex ad. Anim eiusmod do
-                tempor fugiat minim do aliqua amet ex dolore velit.
-              </p>
+        {data.map((item, index) => (
+          <li key={index}>
+            <div className="timeline-date">{item.date}</div>
+            <div className="general-event timeline-event">
+              <div className="info">
+                <h3>{item.title}</h3>
+                <p>{item.text}</p>
+              </div>
             </div>
-          </div>
-        </li>
+          </li>
+        ))}
       </ul>
     </StyledTimeline>
   );
