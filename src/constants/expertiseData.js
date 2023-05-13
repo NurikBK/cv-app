0ebf5b7c52d39172c1d20ec97aa41a055109dae6
Nurzhan Bizhan <bizhannk@gmail.com>
2023-05-13 18:03:41 +0600

    fix typo

--- src/constants/expertiseData.js
+++ src/constants/expertiseData.js
@@ -3,8 +3,8 @@ export const expertiseData = [
     date: '2013-2023',
     info: {
       company: 'Asia Gas Pipeline',
-      job: 'Front-end developer / php programmer',
-      description: `Translate written and spoken content accurately and efficiently from Chinese to English, English to Chinese, Russian to English, and English to Russian
+      job: 'English/Chinese/Kazakh/Russian translator',
+      description: `Translate written and spoken content accurately and efficiently from Chinese/English/Kazakh/Russian to Chinese/English/Kazakh/Russian.
         Proofread and edit translations to ensure accuracy and clarity of the content
         Research industry-specific terminology to ensure that translations are accurate and appropriate
         Use translation software and tools to assist in the translation process
@@ -20,19 +20,12 @@ export const expertiseData = [
       company: 'Bank Center Credit',
       job: 'Junior Web developer',
       description: `Creating and managing content for social media platforms such as Facebook, Twitter, and Instagram.
-
         Writing blog posts and articles for the company website.
-        
         Conducting research and gathering information on industry-related topics to inform content creation.
-        
         Collaborating with the marketing team to create and execute social media and content marketing strategies.
-        
         Managing social media accounts by monitoring engagement and responding to user comments and messages.
-        
         Creating and sending newsletters and email marketing campaigns to subscribers.
-        
-        Conducting website updates and maintenance tasks as needed.
-        
+        Conducting website updates and maintenance tasks as needed.  
         Managing Google Analytics and other marketing performance tracking tools to measure success of content and social media campaigns.
       `,
     },
