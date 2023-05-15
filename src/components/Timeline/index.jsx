e0d1dc94d21cfb53b49231eb38504d1471863862
Nurzhan Bizhan <bizhannk@gmail.com>
2023-05-15 08:16:23 +0600

    fix

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyledTimeline } from '../styles/Timeline.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTimeline } from '../../features/timeline/timelineSlice';
 
-const Timeline = ({ data }) => {
+const Timeline = () => {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.timeline.timeline);
+  const status = useSelector((state) => state.timeline.status);
+  const error = useSelector((state) => state.timeline.error);
+  console.log(data);
+
+  useEffect(() => {
+    dispatch(fetchTimeline());
+  }, [dispatch]);
+
+  if (error) {
+    return <p>{error} </p>;
+  }
+
   return (
     <StyledTimeline>
-      <ul className="timeline-list">
-        {data.map((item, index) => (
-          <li key={index}>
-            <div className="timeline-date">{item.date}</div>
-            <div className="general-event timeline-event">
-              <div className="info">
-                <h3>{item.title}</h3>
-                <p>{item.text}</p>
+      {status === 'loading' ? (
+        <div>Loading...</div>
+      ) : (
+        <ul className="timeline-list">
+          {data.map((item) => (
+            <li key={item?.id}>
+              <div className="timeline-date">{item?.date}</div>
+              <div className="general-event timeline-event">
+                <div className="info">
+                  <h3>{item?.title}</h3>
+                  <p>{item?.text}</p>
+                </div>
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      )}
     </StyledTimeline>
   );
 };
