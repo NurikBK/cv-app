daef041a836ac9f0450f5e4788c6bcaaaa21e458
Nurzhan Bizhan <bizhannk@gmail.com>
2023-05-17 10:40:50 +0600

    remove whitelist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; 
+import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['skills']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
