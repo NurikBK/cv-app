feee304d3b312d3f573ce01a55b2dc834b644dc2
Nurzhan Bizhan <bizhannk@gmail.com>
2023-05-22 17:52:35 +0600

    delete code

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -1,53 +1,25 @@
-import React, { useEffect } from 'react';
 import { StyledTimeline } from '../styles/Timeline.styled';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchTimeline } from '../../features/timeline/timelineSlice';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRotate } from '@fortawesome/free-solid-svg-icons';
 import { Slide } from 'react-awesome-reveal';
 
-const Timeline = () => {
-  const dispatch = useDispatch();
-  const data = useSelector((state) => state.timeline.timeline);
-  const status = useSelector((state) => state.timeline.status);
-  const error = useSelector((state) => state.timeline.error);
+const Timeline = ({data}) => {
 
-  useEffect(() => {
-    if (data.length < 3) {
-      dispatch(fetchTimeline());
-    }
-  }, [dispatch, data]);
-
-  if (status === 'failed' || error) {
-    return (
-      <div className="error">
-        Something went wrong, please review your server connection
-      </div>
-    );
-  }
   return (
     <StyledTimeline>
-      {status === 'loading' ? (
-        <div className="container">
-          <FontAwesomeIcon icon={faRotate} className="rotate" />
-        </div>
-      ) : (
-        <Slide direction="right" triggerOnce={true} cascade={true}>
-          <ul className="timeline-list">
-            {data.map((item) => (
-              <li key={item?.id}>
-                <div className="timeline-date">{item?.date}</div>
-                <div className="general-event timeline-event">
-                  <div className="info">
-                    <h3>{item?.title}</h3>
-                    <p>{item?.text}</p>
-                  </div>
+      <Slide direction="right" triggerOnce={true} cascade={true}>
+        <ul className="timeline-list">
+          {data.map((item, index) => (
+            <li key={index}>
+              <div className="timeline-date">{item.date}</div>
+              <div className="general-event timeline-event">
+                <div className="info">
+                  <h3>{item.title}</h3>
+                  <p>{item.text}</p>
                 </div>
-              </li>
-            ))}
-          </ul>
-        </Slide>
-      )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Slide>
     </StyledTimeline>
   );
 };
